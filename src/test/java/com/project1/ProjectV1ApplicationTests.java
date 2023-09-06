83abb43697752be7202e4f7067c42bf424e2be6e
angela <peekaboo32@naver.com>
2023-09-06 10:15:19 +0900

    라이브러리 버전 충돌  후 다시 재설정

--- src/test/java/com/project1/ProjectV1ApplicationTests.java
+++ src/test/java/com/project1/ProjectV1ApplicationTests.java
@@ -1,6 +1,6 @@
 package com.project1;
 
-import org.apache.ibatis.session.SqlSessionFactory;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,8 +12,7 @@ class ProjectV1ApplicationTests {
 	@Autowired 
 	private ApplicationContext context;
 	
-	@Autowired
-	private SqlSessionFactory sessionFactory;
+
 	@Test
 	void contextLoads() {
 	}
