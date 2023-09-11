97929525e12c5e3ad3121e61e8bad54980346ae2
SUJINJEONG012 <peekaboo32@naver.com>
2023-09-11 21:41:31 +0900

    뷰페이지 설정

---
+++ src/main/java/com/project1/controller/WebController.java
@@ -0,0 +1,15 @@
+package com.project1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+
+	@RequestMapping(value="/")
+	public String main(Model model) {
+		model.addAttribute("template", "fragment/content/main");
+		return "idnex";
+	}
+}
