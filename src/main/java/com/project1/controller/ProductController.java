6f991c6a06d68d9562e997090c7b81c1f0f61028
angela <peekaboo32@naver.com>
2023-09-12 18:00:37 +0900

    오류

---
+++ src/main/java/com/project1/controller/ProductController.java
@@ -0,0 +1,16 @@
+//package com.project1.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class ProductController {
+//
+//	@GetMapping("/product")
+//	public String index(Model model) {
+//	   	model.addAttribute("template","fragments/content/product");
+//	   	return "index";
+//	}
+//  	
+//}
--- src/main/java/com/project1/controller/WebController.java
+++ src/main/java/com/project1/controller/WebController.java
@@ -2,15 +2,22 @@ package com.project1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WebController {
 
-	@RequestMapping(value="/")
-	public String main(Model model) {
-		model.addAttribute("template", "fragments/content/main");
-		return "index";
-	}
+//	@GetMapping("/")
+//	public String main(Model model) {
+//		model.addAttribute("template", "fragments/content/main");
+//		return "index";
+//	}
 
+	@GetMapping("/product")
+	public String index(Model model) {
+	   	model.addAttribute("template","fragments/content/product");
+	   	return "index";
+	}
+  	
 }
