f51e1a3a5cf72c817b1f118664ffe7e490cedc83
SUJINJEONG012 <peekaboo32@naver.com>
2023-09-13 21:13:46 +0900

    CategoryConstant 모델링 테스트에 필요한 클래스

--- src/main/java/com/project1/service/CategoryService.java
+++ src/main/java/com/project1/service/CategoryService.java
@@ -2,10 +2,12 @@ package com.project1.service;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.project1.constant.CategoryConstant;
 import com.project1.domain.Category;
 import com.project1.repository.CategoryRepository;
 
@@ -17,6 +19,11 @@ public class CategoryService {
 
 	public Map<Category, List<Category>> getCategory(){ 
 		List<Category> menuList = categoryRepository.findAll();
-	
+		
+		//parent
+		List<Category> parentMenuList = menuList.stream()
+				.filter(category -> category.getIdParent() == CategoryConstant.PARENT_ROOT)
+				.collect(Collectors.toList());
+		//child
 	}
 }
